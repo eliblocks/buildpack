1e5b2a7049b026b57977d85c32632708282e630a
Eli Block <eliyahu.block@gmail.com>
2020-03-19 18:44:16 -0700

    fix zone

--- profile.d/heroku-google-cloud.sh
+++ profile.d/heroku-google-cloud.sh
@@ -5,5 +5,4 @@ export PATH=/app/vendor/google-cloud-sdk/bin:$PATH
 
 gcloud auth activate-service-account --key-file /app/google-credentials.json
 gcloud config set project metabase-270301
-gcloud config set compute/zone us-central-1
-
+gcloud config set compute/zone us-central1-c
