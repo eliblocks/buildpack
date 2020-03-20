7d393d64dea69bfa32d6ed241d44459d46b23e6f
Eli Block <eliyahu.block@gmail.com>
2020-03-19 19:05:58 -0700

    configure kubectl

--- profile.d/heroku-google-cloud.sh
+++ profile.d/heroku-google-cloud.sh
@@ -6,3 +6,4 @@ export PATH=/app/vendor/google-cloud-sdk/bin:$PATH
 gcloud auth activate-service-account --key-file /app/google-credentials.json
 gcloud config set project metabase-270301
 gcloud config set compute/zone us-central1-c
+gcloud container clusters get-credentials metabase-cluster
