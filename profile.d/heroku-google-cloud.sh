79201e17066597baf885c34a76d424910df92440
Eli Block <eliyahu.block@gmail.com>
2020-03-19 18:28:17 -0700

    fix project id

--- profile.d/heroku-google-cloud.sh
+++ profile.d/heroku-google-cloud.sh
@@ -4,6 +4,6 @@
 export PATH=/app/vendor/google-cloud-sdk/bin:$PATH
 
 gcloud auth activate-service-account --key-file /app/google-credentials.json
-gcloud config set project metabase
+gcloud config set project metabase-270301
 gcloud config set compute/zone us-central-1
 
