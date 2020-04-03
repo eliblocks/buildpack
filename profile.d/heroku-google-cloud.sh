16dfa1cd76d1ea8fbcc728411f1db506739a3280
Eli Block <eliyahu.block@gmail.com>
2020-04-03 11:43:56 -0700

    remove profile.d

--- profile.d/heroku-google-cloud.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Add gcloud to PATH
-export PATH=/app/vendor/google-cloud-sdk/bin:$PATH
-
-gcloud auth activate-service-account --key-file /app/google-credentials.json
-gcloud config set project metabase-270301
-gcloud config set compute/zone us-central1-c
-gcloud container clusters get-credentials metabase-cluster
